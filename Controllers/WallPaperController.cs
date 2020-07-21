bed63cbcc4f533e4827093f5ab80164881cc1de1
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2020-07-21 16:35:51 +0530

    source code added

---
+++ Controllers/WallPaperController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WallPaperAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WallPaperController : ControllerBase
+    {
+        public string[] getData()
+        {
+            return new string[] { "welcome", "Good morning" };
+        }
+    }
+}
